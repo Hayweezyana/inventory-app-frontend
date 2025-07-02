a5b9568eb0bdb59b6b795ecbde90e362ecd2eb51
Ayotunde Adeboyeje <ayotundeadeboyeje@gmail.com>
2025-07-02 21:24:22 +0100

    new commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000/api',
+  baseURL: 'https://inventory-app-backend-08mw.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
